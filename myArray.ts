4e8306e8a8ab9ee40a7fc85fc766201d06ae0be9
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-09-16 17:16:13 +0530

    how the array work in ts

---
+++ myArray.ts
@@ -0,0 +1,7 @@
+const superHeros : string[] = []
+// const superHerosCount : number[] = []
+
+const superHerosCount : Array<number> = []  // Another way to doing the same 
+
+superHeros.push("Spiderman")
+superHerosCount.push(56)
