9e9e99f9420d9bc92fdc653918f0d4ee27b2baf0
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-09-30 11:51:17 +0530

    union

--- myArray.ts
+++ myArray.ts
@@ -6,7 +6,6 @@ const superHerosCount : Array<number> = []  // Another way to doing the same
 superHeros.push("Spiderman")
 superHerosCount.push(56)
 
-
 type Users = {
     name : string;
     isActive : boolean;
@@ -15,3 +14,7 @@ type Users = {
 const AllUsers : Array<Users> = []
 
 AllUsers.push({name:"",isActive:true})
+
+const MLMOdels: number[][] = [
+    [223,43,446]
+]
---
+++ myUnion.ts
@@ -0,0 +1,41 @@
+let  score : number | string = 3
+
+score = 44
+score = "44"
+
+type User = {
+    name:string,
+    id :number
+}
+
+type Admin = {
+    userName:string,
+    id:number
+}
+
+let member : User | Admin = {name:"rhul",id:344}
+
+member
+
+// If i am not checking the what is id type then we get error for we to check it 
+function GetUserId(id:number|string){
+   if(typeof id === "string"){
+     id.toLocaleLowerCase()
+   }else{
+    id +2
+   }
+}
+
+GetUserId("e5")
+
+
+const data1: number[] = [1,2,334]
+const data2: string[] = ["1","45","22"]
+// const data3 : string[] | number[] = [34,56,"34"] //In this case either you can store string or only number 
+
+const data4 : (number|string|boolean)[] = [12,"34",false]  // this is the way to pass multiple types 
+
+let air : "aisle" | "window" | "prime"
+
+air = "aisle"
+// air = "crew" it will now allow 
