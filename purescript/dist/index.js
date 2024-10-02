3f62967fed6cbed60d04556262711d61ceb07a31
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-10-02 22:50:07 +0530

    typescript class

--- purescript/dist/index.js
+++ purescript/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Start of typescript");
+class User {
+    constructor(email, name) {
+        this.city = "Bhadohi";
+        this.name = name,
+            this.email = email;
+    }
+}
+const Ram = new User("ram@gmail.com", "ram");
+// Ram.city ="Bhadohi" 
--- purescript/src/index.ts
+++ purescript/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Start of typescript")
+class User{
+    email:string
+    name : string
+    readonly city:string ="Bhadohi"
+    constructor(email:string,name:string){
+        this.name =  name,
+        this.email = email
+    }
+}
+
+const Ram = new User("ram@gmail.com","ram")
+
+// Ram.city ="Bhadohi" 
