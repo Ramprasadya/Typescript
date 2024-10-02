52039e2851ead861484b3362ce8ebc0eca31cbb3
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-10-02 23:05:38 +0530

    private & public

--- purescript/dist/index.js
+++ purescript/dist/index.js
@@ -2,9 +2,16 @@
 class User {
     constructor(email, name) {
         this.city = "Bhadohi";
-        this.name = name,
-            this.email = email;
+        (this.name = name), (this.email = email);
     }
 }
 const Ram = new User("ram@gmail.com", "ram");
-// Ram.city ="Bhadohi" 
+// Ram.city ="Bhadohi"
+class Form {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+        this.city = "varanasi";
+    }
+}
+let yadav = new Form("rhaul", "rahul@gmail.com");
--- purescript/src/index.ts
+++ purescript/src/index.ts
@@ -1,13 +1,25 @@
-class User{
-    email:string
-    name : string
-    readonly city:string ="Bhadohi"
-    constructor(email:string,name:string){
-        this.name =  name,
-        this.email = email
-    }
+class User {
+  email: string;
+  name: string;
+  readonly city: string = "Bhadohi";
+  constructor(email: string, name: string) {
+    (this.name = name), (this.email = email);
+  }
 }
 
-const Ram = new User("ram@gmail.com","ram")
+const Ram = new User("ram@gmail.com", "ram");
 
-// Ram.city ="Bhadohi" 
+// Ram.city ="Bhadohi"
+
+class Form {
+  readonly city: string = "varanasi";
+  constructor(
+    public name: string,
+    public email: string,
+    // private userId: number
+  ) {
+
+  }
+}
+
+let yadav =  new Form("rhaul","rahul@gmail.com")
