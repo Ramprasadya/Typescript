953475395d26d550a11202961bf441d7a131b8ac
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-10-02 20:52:44 +0530

    Enum

---
+++ myEnum.ts
@@ -0,0 +1,9 @@
+enum seatChoice  {
+    ASILE ="AISILE", // if you give the string then you have aloow for rest item too , If you only want to set frist as a string and other as number than you do like that
+    MIDDLE=2,
+    PRO,
+    ECONOMY
+}
+
+
+const hcSeat =  seatChoice.MIDDLE
