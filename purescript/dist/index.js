bdfee81ba4ec75f209ad203f9390e10d8915ce5b
Ramprasad yadav <yadavramprasad563@gmail.com>
2024-10-05 22:11:07 +0530

    getter & setter

--- purescript/dist/index.js
+++ purescript/dist/index.js
@@ -11,7 +11,22 @@ class Form {
     constructor(name, email) {
         this.name = name;
         this.email = email;
+        this._courseCount = 1;
         this.city = "varanasi";
+    }
+    get getAppleEmail() {
+        return `apple value ${this.email}`;
+    }
+    // getter
+    get courseCount() {
+        return this._courseCount; // we can not get direct value of a private value , for getting value we use getter 
+    }
+    // setter
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("CourseNum can should be 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 let yadav = new Form("rhaul", "rahul@gmail.com");
--- purescript/src/index.ts
+++ purescript/src/index.ts
@@ -12,6 +12,9 @@ const Ram = new User("ram@gmail.com", "ram");
 // Ram.city ="Bhadohi"
 
 class Form {
+
+  private _courseCount = 1;
+
   readonly city: string = "varanasi";
   constructor(
     public name: string,
@@ -20,6 +23,28 @@ class Form {
   ) {
 
   }
+
+
+  get getAppleEmail():string{
+    return `apple value ${this.email}`
+  }
+  // getter
+
+  get courseCount():number{
+    return this._courseCount  // we can not get direct value of a private value , for getting value we use getter 
+  }
+
+  // setter
+
+  set courseCount(courseNum){
+    if(courseNum <=1){
+      throw new Error ("CourseNum can should be 1")
+    }
+
+    this._courseCount = courseNum
+
+  }
+
 }
 
 let yadav =  new Form("rhaul","rahul@gmail.com")
